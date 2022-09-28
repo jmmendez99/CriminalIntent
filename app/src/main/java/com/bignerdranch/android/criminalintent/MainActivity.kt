bc641f0be33888cca420f2c3615fc42f815e208f
Justin Mendez <jmmendez@cpp.edu>
2022-09-27 23:41:56 -0700

    Finish Chapter 8 content

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.bignerdranch.android.criminalintent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Getting the current fragment
+        val currentFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_container)
+
+        //If the fragment doesn't exist, then we create it and
+        //then use this activity to host it in a fragment container
+        //in activity_main.xml
+        if (currentFragment == null) {
+            val fragment = CrimeFragment()
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragment_container, fragment)
+                .commit()
+        }
+    }
+}
