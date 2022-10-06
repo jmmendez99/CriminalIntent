3f079bdd2aa68168ab8e89f79bcb864d8c99f97b
Justin Mendez <jmmendez@cpp.edu>
2022-10-06 15:45:07 -0700

    Add more comments about typeconverters

--- app/src/main/java/database/CrimeTypeConverters.kt
+++ app/src/main/java/database/CrimeTypeConverters.kt
@@ -9,11 +9,16 @@ class CrimeTypeConverters {
     //putting the data into our Crime database and to also
     //handle retrieving the data and converting it
     //back to its original form before it was in the database.
+
+    //Converts Date object to Long data type since the database
+    //knows how to deal with longs
     @TypeConverter
     fun fromDate(date: Date?): Long? {
         return date?.time
     }
 
+    //Coverts long data type to a Date object, which is our
+    //original date
     @TypeConverter
     fun toDate(millisSinceEpoch: Long?): Date? {
         return millisSinceEpoch?.let {
@@ -23,11 +28,15 @@ class CrimeTypeConverters {
 
     //the process in the comment above explains the same
     //process that should be done for the UUID.
+
+    //converts string to UUID object, which is our original data
     @TypeConverter
     fun toUUID(uuid: String?): UUID? {
         return UUID.fromString(uuid)
     }
 
+    //converts UUID to a string since the database can handle
+    //that data type
     @TypeConverter
     fun fromUUID(uuid: UUID?): String? {
         return uuid?.toString()
