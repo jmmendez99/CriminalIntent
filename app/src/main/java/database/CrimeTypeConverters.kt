62bc71ec90e0124eb60bb9b708e3e730d1e7b8c4
Justin Mendez <jmmendez@cpp.edu>
2022-10-06 15:18:05 -0700

    Add comment about why we use typeconverters

--- app/src/main/java/database/CrimeTypeConverters.kt
+++ app/src/main/java/database/CrimeTypeConverters.kt
@@ -5,6 +5,10 @@ import java.util.*
 
 class CrimeTypeConverters {
 
+    //We need these two different implementations to handle
+    //putting the data into our Crime database and to also
+    //handle retrieving the data and converting it
+    //back to its original form before it was in the database.
     @TypeConverter
     fun fromDate(date: Date?): Long? {
         return date?.time
@@ -17,6 +21,8 @@ class CrimeTypeConverters {
         }
     }
 
+    //the process in the comment above explains the same
+    //process that should be done for the UUID.
     @TypeConverter
     fun toUUID(uuid: String?): UUID? {
         return UUID.fromString(uuid)
