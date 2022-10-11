41511b709dd5ab50ebb5d898923803448ef8c2b9
Justin Mendez <jmmendez@cpp.edu>
2022-10-11 13:11:35 -0700

    Add comment to explain how CrimeDatabase.kt and its DAO work

--- app/src/main/java/database/CrimeDatabase.kt
+++ app/src/main/java/database/CrimeDatabase.kt
@@ -9,5 +9,8 @@ import com.bignerdranch.android.criminalintent.Crime
 @TypeConverters(CrimeTypeConverters::class)
 abstract class CrimeDatabase : RoomDatabase() {
 
+    //we do this so that Room will handle generating a concrete
+    //version of the CrimeDao class. This line of code tells our
+    //database class to generate an instance of the DAO.
     abstract fun crimeDao(): CrimeDao
 }
