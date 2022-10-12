8bcaf1af980ef26ec70da20370e058c54f3196c7
Justin Mendez <jmmendez@cpp.edu>
2022-10-11 17:27:38 -0700

    Change CrimeDao.kt by making queries return LiveData objects that wrap original data's type

--- app/src/main/java/database/CrimeDao.kt
+++ app/src/main/java/database/CrimeDao.kt
@@ -1,5 +1,6 @@
 package database
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Query
 import com.bignerdranch.android.criminalintent.Crime
@@ -11,6 +12,12 @@ import java.util.*
 
 @Dao
 interface CrimeDao {
+    //We change the return type of the query to a liveData object
+    //that wraps the original return type. We do this so that we
+    //can use a background thread to get data from our database
+    //and then let the main thread know that those results are
+    //ready to be retrieved.
+
 
     //The @Query annotation expects a string
     //containing a SQL command as input.
@@ -18,12 +25,12 @@ interface CrimeDao {
     //SELECT * FROM crime asks Room to pull
     // all columns for all rows in the crime database table.
     @Query("SELECT * FROM crime")
-    fun getCrimes(): List<Crime>
+    fun getCrimes(): LiveData<List<Crime>>
 
 
     //SELECT * FROM crime WHERE id=(:id) asks Room to pull
     //all columns from only the row whose id matches
     //the ID value provided.
     @Query("SELECT * FROM crime WHERE id=(:id)")
-    fun getCrime(id: UUID): Crime?
+    fun getCrime(id: UUID): LiveData<Crime?>
 }
